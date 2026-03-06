d6572545e3ec4090797c50474979b9c8f7163851
hamzahhamlh <132518351+hamzahhamlh@users.noreply.github.com>
2026-03-07 01:33:50 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,5 @@
+// --- Supabase Configuration ---
+// !! IMPORTANT: Replace with your actual project values !!
+const SUPABASE_URL = "https://txkjrpjtnsfmowvruavv.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_LTjbzxsycVTPxUwJtxdB4g_g0pS6GXN";
+
